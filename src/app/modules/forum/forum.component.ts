94312c869b79b377616ad24787e744bf7dccf7b1
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-03-09 03:16:16 +0700

    create new component for main forum

--- src/app/modules/forum/forum.component.ts
+++ src/app/modules/forum/forum.component.ts
@@ -7,10 +7,11 @@ import { TabViewService } from '../../shared/component/layout/tabview.service';
   styleUrl: './forum.component.scss'
 })
 export class ForumComponent {
+  isExtra: boolean = false;
 
   constructor(
     private tabViewService: TabViewService,
-  ) { 
+  ) {
     this.tabViewService.currentComponent.subscribe(component => {
       if (component == 'forum') {
         this.load();
@@ -20,5 +21,9 @@ export class ForumComponent {
 
   load() {
     console.log('load forum component');
+  }
+
+  onChangeState(){
+    this.isExtra = !this.isExtra;
   }
 }
