886c7e5319d058a2dc8c4a4630bb0a2eaf9e8f56
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-03-09 03:16:00 +0700

    create new component for forum create

---
+++ src/app/modules/forum/forum-create/forum-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forum-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './forum-create.component.html',
+  styleUrl: './forum-create.component.scss'
+})
+export class ForumCreateComponent {
+
+}
