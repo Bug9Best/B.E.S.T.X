7cb4f79128479266e9beeacbf5ce863c0964d722
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-03-09 03:14:23 +0700

    create new component for course detail

---
+++ src/app/modules/course/course-detail/course-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-detail',
+  templateUrl: './course-detail.component.html',
+  styleUrl: './course-detail.component.scss'
+})
+export class CourseDetailComponent {
+
+}
