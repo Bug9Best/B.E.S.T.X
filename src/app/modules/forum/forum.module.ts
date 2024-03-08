dd3f8fb68a9604bfdb950600209026284e7d2c0e
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-03-09 03:14:39 +0700

    update module

--- src/app/modules/forum/forum.module.ts
+++ src/app/modules/forum/forum.module.ts
@@ -2,27 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-import { SharedModule } from 'primeng/api';
 import { PrimeNGModule } from '../../shared/primeng.module';
 import { ForumComponent } from './forum.component';
+import { ForumListComponent } from './forum-list/forum-list.component';
+import { ForumControlComponent } from './forum-control/forum-control.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ForumComponent,
   },
+ 
 ];
 
 @NgModule({
   declarations: [
-    ForumComponent
+    ForumComponent,
+    ForumListComponent,
+    ForumControlComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     PrimeNGModule,
-    SharedModule,
     RouterModule.forChild(routes),
   ],
   exports: [
