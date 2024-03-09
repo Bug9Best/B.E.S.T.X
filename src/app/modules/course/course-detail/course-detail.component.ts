6a6fcf6064f5efdf0d971ea1ab288e75736cf0f6
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-03-09 12:52:27 +0700

    update course detail component

--- src/app/modules/course/course-detail/course-detail.component.ts
+++ src/app/modules/course/course-detail/course-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'course-detail',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class CourseDetailComponent {
 
+  public items: MenuItem[] = this.initMenu();
+
+  private initMenu() {
+    return [
+      { label: 'หน้าแรก', routerLink: '/course' },
+      { label: 'รายละเอียดรายวิชา' },
+    ];
+  }
+
+  regisCourse() {
+    
+  }
 }
