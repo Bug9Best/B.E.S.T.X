2633bc3ba9f9bb55f14b99e8125f2804a20615e3
Natthaphon Ditthaviboon <88504995+Bug9Best@users.noreply.github.com>
2024-03-09 10:58:49 +0700

    update navigation bar

--- src/app/shared/component/layout/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/component/layout/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Menu } from '../../../../config/menu';
 import { TabPanel } from 'primeng/tabview';
 import { HomeComponent } from '../../../../modules/home/home.component';
@@ -11,51 +11,37 @@ import { NotFoundComponent } from '../../not-found/not-found.component';
 import { TabViewService } from '../tabview.service';
 import { CalendarComponent } from '../../../../modules/calendar/calendar.component';
 import { ProfileComponent } from '../../../../modules/profile/profile.component';
+import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
-export class NavigationBarComponent {
-  listMenus: any[] = Menu;
+export class NavigationBarComponent implements OnInit {
+
+  listMenus: MenuItem[] | undefined;
+  activeItem: MenuItem | undefined;
 
   constructor(
+    private router: Router,
     private tabViewService: TabViewService,
   ) {
-    this.tabViewService.currentComponent.subscribe((componentName: string) => {
-      this.getComponent(componentName);
-    });
+    this.tabViewService.currentComponent
+      .subscribe((componentName: string) => {
+        this.router.navigate(['/', componentName]);
+        this.activeItem = this.listMenus?.find((menu: any) => menu.component === componentName);
+      });
   }
 
-  getComponent(componentName: string): any {
-    switch (componentName) {
-      case 'home':
-        return HomeComponent;
-      case 'profile':
-        return ProfileComponent;
-      case 'course':
-        return CourseComponent;
-      case 'calendar':
-        return CalendarComponent;
-      case 'forum':
-        return ForumComponent;
-      case 'chat':
-        return ChatComponent;
-      case 'playground':
-        return PlaygroundComponent;
-      case 'streaming':
-        return StreamingComponent;
-      default:
-        return NotFoundComponent;
-    }
+  ngOnInit(): void {
+    this.listMenus = Menu
+    this.router.navigate(['/home']);
+    this.activeItem = this.listMenus[0];
   }
 
-  @ViewChildren(TabPanel) tabPanels?: QueryList<TabPanel>;
-  tabChange(index: any) {
-    if (!this.tabPanels) return;
-    let tabPanel = this.tabPanels.get(index) as any;
-    let tag = tabPanel.tag;
-    this.tabViewService.setCurrentComponent(tag);
+  onChaneTab(menu: any) {
+    this.tabViewService.setCurrentComponent(menu.component);
   }
 }
